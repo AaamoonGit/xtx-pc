b203b5fd0753336a0a921454711ce673cd79c922
Aaamoon <16471570+aaamoongitee@user.noreply.gitee.com>
2026-03-24 20:38:44 +0800

    配置husky+lint-staged~1

--- eslint.config.js
+++ eslint.config.js
@@ -9,6 +9,17 @@ export default defineConfig([
   {
     name: 'app/files-to-lint',
     files: ['**/*.{vue,js,mjs,jsx}'],
+    rules: {
+      'vue/multi-word-component-names': [
+        'warn',
+        {
+          ignores: ['index'] // vue组件名称多单词组成（忽略index.vue）
+        }
+      ],
+      'vue/no-setup-props-destructure': ['off'], // 关闭 props 解构的校验
+      // 💡 添加未定义变量错误提示，create-vue@3.6.3 关闭，这里加上是为了支持下一个章节演示。
+      'no-undef': 'error'
+    }
   },
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
@@ -16,9 +27,9 @@ export default defineConfig([
   {
     languageOptions: {
       globals: {
-        ...globals.browser,
-      },
-    },
+        ...globals.browser
+      }
+    }
   },
 
   js.configs.recommended,
@@ -26,5 +37,5 @@ export default defineConfig([
 
   ...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
 
-  skipFormatting,
+  skipFormatting
 ])
--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
 <script setup>
+const obj = {
+  a: 1
+}
+console.log(obj)
 </script>
 <template>
   <div>123</div>
