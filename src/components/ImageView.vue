d4df28faa22d47afe96f99059ad673e584df790d
Aaamoon <16471570+aaamoongitee@user.noreply.gitee.com>
2026-04-09 15:50:46 +0800

    放大镜效果完成

--- src/components/ImageView.vue
+++ src/components/ImageView.vue
@@ -7,23 +7,86 @@ const imageList = [
   'https://yanxuan-item.nosdn.127.net/f93243224dc37674dfca5874fe089c60.jpg',
   'https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg'
 ]
-import { ref } from 'vue'
+import { ref, useTemplateRef } from 'vue'
 const activeIndex = ref(0)
 const changeLarge = (i) => {
   // console.log(i);
   activeIndex.value = i
+}
+
+const isLargeVisible = ref(false)
+
+import { useMouseInElement } from '@vueuse/core'
+const target = useTemplateRef('target')
+const { elementX, elementY, isOutside, elementHeight, elementWidth } =
+  useMouseInElement(target)
+const layer = useTemplateRef('layerRef')
+const layerleft = ref(0)
+const layerTop = ref(0)
+const bcpX = ref(0)
+const bcpY = ref(0)
+const isFirstMove = ref(true)
+let timer = null
+const middleChange = () => {
+  if (isFirstMove.value) {
+    timer = setTimeout(() => {
+      isFirstMove.value = false
+    }, 200)
+  }
+
+  // 放大元素显示
+  isLargeVisible.value = true
+  console.log(isOutside.value)
+
+  console.log(elementX.value, elementY.value, isOutside.value)
+
+  // 滑块移动
+  layerleft.value = elementX.value - layer.value.offsetWidth / 2
+  layerTop.value = elementY.value - layer.value.offsetHeight / 2
+
+  // 边界控制
+  if (elementX.value + layer.value.offsetWidth / 2 >= elementHeight.value) {
+    layerleft.value = elementHeight.value - layer.value.offsetWidth
+  }
+  if (elementY.value + layer.value.offsetHeight / 2 >= elementWidth.value) {
+    layerTop.value = elementWidth.value - layer.value.offsetHeight
+  }
+  if (elementX.value <= layer.value.offsetWidth / 2) {
+    layerleft.value = 0
+  }
+  if (elementY.value <= layer.value.offsetHeight / 2) {
+    layerTop.value = 0
+  }
+
+  // 放大图移动
+  bcpX.value = -layerleft.value * 2
+  bcpY.value = -layerTop.value * 2
+}
+const middleLeave = () => {
+  isLargeVisible.value = false
+  isFirstMove.value = true
+  clearTimeout(timer)
 }
 </script>
 
 <template>
   <div class="goods-image">
     <!-- 左侧大图-->
-    <div class="middle" ref="target">
+    <div
+      class="middle"
+      ref="target"
+      @mousemove="middleChange"
+      @mouseleave="middleLeave"
+    >
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div
+        ref="layerRef"
+        class="layer"
+        :class="{ firstMove: isFirstMove }"
+        :style="{ left: `${layerleft}px`, top: `${layerTop}px` }"
+      ></div>
     </div>
-    activeIndex
     <!-- 小图列表 -->
     <ul ref="smallImgRef" class="small">
       <li
@@ -40,12 +103,12 @@ const changeLarge = (i) => {
       class="large"
       :style="[
         {
-          backgroundImage: `url(${imageList[0]})`,
-          backgroundPositionX: `0px`,
-          backgroundPositionY: `0px`
+          backgroundImage: `url(${imageList[activeIndex]})`,
+          backgroundPositionX: `${bcpX}px`,
+          backgroundPositionY: `${bcpY}px`
         }
       ]"
-      v-show="false"
+      v-show="isLargeVisible"
     ></div>
   </div>
 </template>
@@ -85,6 +148,9 @@ const changeLarge = (i) => {
     left: 0;
     top: 0;
     position: absolute;
+    &.firstMove {
+      transition: all 0.5s ease;
+    }
   }
 
   .small {
